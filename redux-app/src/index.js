8fdc0e06ca339409e121367d58a4f9e48b2fb5d4
Nicholas Klufas <nick@klufas.me>
2019-07-07 15:21:41 -0400

    Adding ehancers and middleware

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducers'
+import loggerMiddleware from './middleware/logger'
+import monitorReducerEnhancer from './enhancers/monitorReducer'
 import App from './components/App'
 
-const store = createStore(rootReducer)
+const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware)
+const composedEnhancers = compose(
+  middlewareEnhancer,
+  monitorReducerEnhancer
+)
+
+const store = createStore(rootReducer, undefined, composedEnhancers)
 
 render(
   <Provider store={store}>
